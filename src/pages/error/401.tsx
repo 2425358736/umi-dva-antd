846cd6263f49a8a622c9a757b9bbdf7a98373fdd
xx_apple_liu <liuning_apple@126.com>
2018-10-21 20:46:31 +0800

    login's styles

--- src/pages/error/401.tsx
+++ src/pages/error/401.tsx
@@ -1,8 +1,9 @@
+const errorStyled = require('./error.less')
 
 export default function () {
   return (
     <>
-    <div>
+    <div className={ errorStyled.status }>
       <img style={{width: '100%'}} alt={500} src={require('../../../src/assets/401.jpg')}/>
     </div>
     </>
